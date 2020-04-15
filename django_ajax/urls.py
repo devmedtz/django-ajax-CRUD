509b4ba0c5fb2b5f3385aec6dd9ac1bf9a78daf6
fagrimacs <58547071+fagrimacs@users.noreply.github.com>
2020-04-15 22:12:25 +0300

    initial commit

--- django_ajax/urls.py
+++ django_ajax/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
+    path('books/', include('books.urls', namespace='books')),
     path('admin/', admin.site.urls),
 ]
 
